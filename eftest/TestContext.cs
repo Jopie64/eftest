2c863417b8a6f9e1b883c287d68252a9d7133d37
Johan 't Hart <johan.thart@anywhere365.net>
2025-05-26 10:03:29 +0200

    Add table to removed and migration

--- eftest/TestContext.cs
+++ eftest/TestContext.cs
@@ -9,9 +9,18 @@ public class Test
     public string Name { get; set; }
 }
 
+public class TestToBeRemoved
+{
+    public int Id { get; set; }
+    public int TestId { get; set; }
+
+    public Test Test { get; set; }
+}
+
 public class TestContext : DbContext
 {
     public DbSet<Test> Tests { get; set; }
+    public DbSet<TestToBeRemoved> TestToBeRemoved { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite("Data Source=./MyTestDb.sqlite");
---
+++ eftest/migrations/20250526083615_AddTestToBeRemoved.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eftest.migrations
+{
+    /// <inheritdoc />
+    public partial class AddTestToBeRemoved : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TestToBeRemoved",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    TestId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TestToBeRemoved", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TestToBeRemoved_Tests_TestId",
+                        column: x => x.TestId,
+                        principalTable: "Tests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TestToBeRemoved_TestId",
+                table: "TestToBeRemoved",
+                column: "TestId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TestToBeRemoved");
+        }
+    }
+}
