f43b2b7373f34d699061dab077ce5b4b05b29640
Johan 't Hart <johan.thart@anywhere365.net>
2025-05-26 09:54:20 +0200

    Add table and run migrations

---
+++ eftest/TestContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Eftest;
+
+
+public class Test
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class TestContext : DbContext
+{
+    public DbSet<Test> Tests { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EfTestDb;Trusted_Connection=True;");
+}
