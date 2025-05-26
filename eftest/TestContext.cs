33478cd649f2d901adafade660f887b5c58440df
Johan 't Hart <johan.thart@anywhere365.net>
2025-05-26 10:45:44 +0200

    Remove table to be removed

--- eftest/TestContext.cs
+++ eftest/TestContext.cs
@@ -9,19 +9,9 @@ public class Test
     public string NameChanged { get; set; }
 }
 
-public class TestToBeRemoved
-{
-    public int Id { get; set; }
-    public int TestId { get; set; }
-    public string NameChanged { get; set; }
-
-    public Test Test { get; set; }
-}
-
 public class TestContext : DbContext
 {
     public DbSet<Test> Tests { get; set; }
-    public DbSet<TestToBeRemoved> TestToBeRemoved { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         => options.UseSqlite("Data Source=./MyTestDb.sqlite");
