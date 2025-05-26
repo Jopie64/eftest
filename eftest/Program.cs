395460a36b140ea826e4bb36d58d79cefdd077e5
Johan 't Hart <johan.thart@anywhere365.net>
2025-05-26 09:54:20 +0200

    Add table and run migrations

--- eftest/Program.cs
+++ eftest/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Eftest;
+using Microsoft.EntityFrameworkCore;
+
+try
+{
+    using var db = new TestContext();
+    db.Database.Migrate();
+    Console.WriteLine("Database migrated successfully.");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while migrating the database: {ex.Message}");
+    Console.WriteLine(ex.StackTrace);
+}
---
+++ eftest/TestContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Eftest;
+
+
+public class Test
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class TestContext : DbContext
+{
+    public DbSet<Test> Tests { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("Data Source=./MyTestDb.sqlite");
+}
