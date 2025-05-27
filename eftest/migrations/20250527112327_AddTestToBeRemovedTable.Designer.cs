ebde74df50ff812234fcd0d04132229a30e1af55
Johan 't Hart <johan.thart@anywhere365.net>
2025-05-27 13:24:40 +0200

    Add table to removed and migration

--- eftest/migrations/20250527112327_AddTestToBeRemovedTable.Designer.cs
+++ eftest/migrations/20250527112327_AddTestToBeRemovedTable.Designer.cs
@@ -3,6 +3,7 @@ using Eftest;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace eftest.migrations
 {
     [DbContext(typeof(TestContext))]
-    partial class TestContextModelSnapshot : ModelSnapshot
+    [Migration("20250527112327_AddTestToBeRemovedTable")]
+    partial class AddTestToBeRemovedTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
