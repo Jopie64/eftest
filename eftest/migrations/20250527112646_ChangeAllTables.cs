040d13038646768037276ad28ef9301172a66aeb
Johan 't Hart <johan.thart@anywhere365.net>
2025-05-27 13:30:31 +0200

    Change all tables

---
+++ eftest/migrations/20250527112646_ChangeAllTables.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eftest.migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAllTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Tests",
+                newName: "NameChanged");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "TestToBeRemoved",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "TestToBeRemoved");
+
+            migrationBuilder.RenameColumn(
+                name: "NameChanged",
+                table: "Tests",
+                newName: "Name");
+        }
+    }
+}
